200917adb122b9dd1e8ce86dc26a5cb4d0e766b1
Kevin Unkle <kevinunk@gmail.com>
2023-06-14 21:21:50 +0000

    start of project

--- js/data.js
+++ js/data.js
@@ -1,1 +1,23 @@
 /* exported data */
+var card = {
+  name: '',
+  hp: '',
+  attack0: '',
+  attack1: '',
+  weakness: '',
+  resistance: '',
+  retreatCost: ''
+};
+
+function getPokemonCardData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.pokemontcg.io/v2/cards?q=set.id:base1');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    const baseSet = xhr.response;
+    console.log(baseSet);
+  });
+  xhr.send();
+}
+getPokemonCardData();
